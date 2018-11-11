6595282177a53f3165b9d47b19aefa07e2f51873
Sami Lahtinen <thesamsai@gmail.com>
2018-11-11 19:43:00 +0200

    Poistettiin annotaatiot (siirretty branchiin annotaatio)

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
@@ -4,7 +4,6 @@ package ohtu.verkkokauppa;
 import java.util.ArrayList;
 import org.springframework.stereotype.Component;
 
-@Component
 public class Kirjanpito implements KirjanpitoInterface {
     private ArrayList<String> tapahtumat;
 
